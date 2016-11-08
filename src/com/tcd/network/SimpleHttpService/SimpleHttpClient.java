4b500c1550e9e3030ef923b40e9a4bb6de1daae2
Sagar Sachdeva <sagarsachdeva@Sagars-MacBook-Pro.local>
2016-11-08 21:21:34 +0000

    Simple HTTP Client Initial commit

---
+++ src/com/tcd/network/SimpleHttpService/SimpleHttpClient.java
@@ -0,0 +1,57 @@
+package com.tcd.network.SimpleHttpService;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+public class SimpleHttpClient {
+
+	private static String url = "http://192.168.0.49:8000/php-project/echo.php?message=Kya_kr_re_ho";
+
+	public static void main(String[] args) {
+		// Create an instance of HttpClient.
+		HttpClient client = new HttpClient();
+
+		// Create a method instance.
+		GetMethod method = new GetMethod(url);
+
+		// Provide custom retry handler is necessary
+//		method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
+
+		try {
+			// Execute the method.
+			int statusCode = client.executeMethod(method);
+
+			if (statusCode != HttpStatus.SC_OK) {
+				System.err.println("Method failed: " + method.getStatusLine());
+			}
+
+			// Read the response body.
+			byte[] responseBody = new byte[1024];
+			InputStream stream = method.getResponseBodyAsStream();
+			stream.read(responseBody);
+
+			// Deal with the response.
+			// Use caution: ensure correct character encoding and is not binary
+			// data
+			System.out.println(new String(responseBody));
+
+		} catch (HttpException e) {
+			System.err.println("Fatal protocol violation: " + e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Fatal transport error: " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Fatal transport error: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			// Release the connection.
+			method.releaseConnection();
+		}
+	}
+}
